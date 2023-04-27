dd999baa07feb2cd105ef1f90f80c39c36286989
rickardthunborg <rickard.a.thunborg@gmail.com>
2023-04-27 13:41:47 +0200

    Initial commit

---
+++ Project Hashtag/Controllers/APIController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_Hashtag.Data;
+
+namespace Project_Hashtag.Controllers
+{
+    [Route("/api")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        private readonly AppDbContext database;
+
+        public APIController(AppDbContext database)
+        {
+            this.database = database;
+        }
+    }
+}
---
+++ Project Hashtag/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_Hashtag.Models;
+using System.Collections.Generic;
+
+namespace Project_Hashtag.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
