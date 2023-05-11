8fd451f98cfd5e5165cc3bc34c4ac81812d597e2
GustavM01 <gustav.milihagulu@gmail.com>
2023-05-11 11:54:02 +0200

    ProfileSettings

---
+++ Project Hashtag/Pages/ProfileSettings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project_Hashtag.Pages
+{
+    public class ProfileSettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
