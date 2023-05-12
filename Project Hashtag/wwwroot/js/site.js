f52cda93509abc54dde11ef7d8837ee2815b55ea
alexxgru <alexgrundell@hotmail.com>
2023-05-12 12:24:52 +0200

    post field changes

--- Project Hashtag/wwwroot/js/site.js
+++ Project Hashtag/wwwroot/js/site.js
@@ -1,4 +1,18 @@
 ﻿function autoExpand(textarea) {
         textarea.style.height = "auto";
         textarea.style.height = textarea.scrollHeight + "px";
+}
+
+const fileInput = document.getElementById('photo-input');
+const fileLabel = document.getElementById('file-label');
+
+fileInput.addEventListener('change', function () {
+    if (fileInput.files.length > 0) {
+        const checkmark = document.createElement('img');
+        checkmark.src = '../done.png';
+        checkmark.alt = 'Done';
+
+        // Append the checkmark image
+        fileLabel.appendChild(checkmark);
     }
+});
